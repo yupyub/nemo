2b52bb1af4ded54b1421cd00f5f7c5efaf8014dc
yupyub <sungyup5003@gmail.com>
2018-11-16 23:02:16 +0900

    boj

--- ML/ex2/predict.m
+++ ML/ex2/predict.m
@@ -14,11 +14,8 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
+A = sigmoid(X*theta);
+p = round(A);
 
 
 % =========================================================================
