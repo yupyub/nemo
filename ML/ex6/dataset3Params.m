5b716c44d654071668f900a9dac62e051b394d4a
yupyub <sungyup5003@gmail.com>
2019-01-10 15:19:22 +0900

    boj

--- ML/ex6/dataset3Params.m
+++ ML/ex6/dataset3Params.m
@@ -23,11 +23,19 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+err = 100000000;
+for tmpc = [0.01,0.03,0.1,0.3,1,3,10,30]
+	for tmps = [0.01,0.03,0.1,0.3,1,3,10,30]
+		model = svmTrain(X, y, tmpc, @(x1, x2) gaussianKernel(x1, x2, tmps));
+		e = mean(double(svmPredict(model,Xval) ~= yval));
+		if(e<err)
+			err = e;
+			C = tmpc;
+			sigma = tmps;
+		end
+	end	
+end
+		
 
 % =========================================================================
 
